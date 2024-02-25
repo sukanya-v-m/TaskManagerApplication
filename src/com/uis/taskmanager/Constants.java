dc36bd9b638beccda8f6c32c9012eda8f0405145
sukanya <madiwalarsukanya@gmail.com>
2024-02-25 19:50:21 +0530

    TaskManagerApp commit

---
+++ src/com/uis/taskmanager/Constants.java
@@ -0,0 +1,10 @@
+package com.uis.taskmanager;
+
+import java.io.File;
+
+public interface Constants {
+	int PRIORITY1 = 1;
+	int PRIORITY2 = 5;
+	int PRIORITY3 = 10;
+	File PATH = new File("D:\\Sukanya Eclipse\\TaskManagerAppFinal\\DetailsOfApp");
+}
